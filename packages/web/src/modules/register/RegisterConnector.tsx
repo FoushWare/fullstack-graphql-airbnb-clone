ab5e1edb39097e675c1b49d72fd2e9a65de53a79
foushware <mr.fouad992@gmail.com>
2020-07-16 23:35:18 +0200

    :hamster: adding a controlelr package to share code between React and ReactNative

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,17 +1,19 @@
 import * as React from "react";
 import { RegisterView } from "./ui/RegisterView";
+import { RegisterController } from "@abb/controller";
 
 // container -> view
 // container -> connector -> view
 // controller -> connector -> view
 
 export class RegisterConnector extends React.PureComponent {
-  dummySubmit = async (values: any) => {
-    console.log(values);
-    return null;
-  };
+ 
 
   render() {
-    return <RegisterView submit={this.dummySubmit} />;
+    return(
+      <RegisterController>
+        {({submit})=> <RegisterView submit={submit}/>}
+      </RegisterController>
+    )
   }
 }
