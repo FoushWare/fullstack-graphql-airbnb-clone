97bfc27760c131eb0bf486a8738944535af90c86
foushware <mr.fouad992@gmail.com>
2020-07-18 13:42:20 +0200

    :alien: call register mutation

--- packages/controller/src/modules/RegisterController/index.ts
+++ packages/controller/src/modules/RegisterController/index.ts
@@ -1,20 +1,39 @@
 
 import * as React from 'react';
+import { graphql, ChildMutateProps } from "react-apollo";
+import gql from "graphql-tag";
 
-interface Props{
-    children:(
-       data:{submit:(values:any)=>Promise<null>} 
-    )=>JSX.Element | null;
+interface Props {
+    children: (
+        data: { submit: (values: any) => Promise<null> }
+    ) => JSX.Element | null;
 }
 
-export class RegisterController extends React.PureComponent<Props>{
-    submit = async(values:any)=>{
+class C extends React.PureComponent<ChildMutateProps<Props, any, any>>{
+    submit = async (values: any) => {
         console.log(values);
+        const response = await this.props.mutate({
+            variables: values
+        });
+        console.log(response);
         return null;
     };
 
-    render(){
-        return this.props.children({submit:this.submit});
+
+    render() {
+        return this.props.children({ submit: this.submit });
     }
 
 }
+const registerMutation = gql`
+    
+    mutation($email: String!, $password: String!) {
+    register(email: $email, password: $password) {
+      path
+      message
+    }
+  }
+    
+    
+    `;
+export const RegisterController = graphql(registerMutation)(C)
