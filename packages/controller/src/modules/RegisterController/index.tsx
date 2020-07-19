640271df86d473ddae21f696d1a485fc086dd74d
foushware <mr.fouad992@gmail.com>
2020-07-19 04:08:32 +0200

    :poop: use apollo codegen to generate types

---
+++ packages/controller/src/modules/RegisterController/index.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { graphql, ChildMutateProps } from "react-apollo";
+import gql from "graphql-tag";
+import { RegisterMutationVariables, RegisterMutation } from "src/operation-result-types";
+
+
+interface Props {
+  children: (
+    data: { submit: (values: RegisterMutationVariables) => Promise<null> }
+  ) => JSX.Element | null;
+}
+
+class C extends React.PureComponent<
+  ChildMutateProps<Props, RegisterMutation, RegisterMutationVariables>
+> {
+  submit = async (values: RegisterMutationVariables) => {
+    console.log(values);
+    const response = await this.props.mutate({
+      variables: values
+    });
+    console.log("response: ", response);
+    return null;
+  };
+
+  render() {
+    return this.props.children({ submit: this.submit });
+  }
+}
+
+const registerMutation = gql`
+  mutation RegisterMutation($email: String!, $password: String!) {
+    register(email: $email, password: $password) {
+      path
+      message
+    }
+  }
+`;
+
+export const RegisterController = graphql<
+  Props,
+  RegisterMutation,
+  RegisterMutationVariables
+>(registerMutation)(C);
